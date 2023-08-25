b7320524f09b645be23d3a999504bf2b595fa609
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-08-25 14:24:28 -0300

    Implementação da interface

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import model.Product;
+import util.ProductPredicate;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        List<Product> productsList = new ArrayList<>();
+
+        productsList.add(new Product("Tv", 900.00));
+        productsList.add(new Product("Mouse", 50.00));
+        productsList.add(new Product("Tablet", 350.00));
+        productsList.add(new Product("Hd Case", 80.90));
+
+        productsList.removeIf(new ProductPredicate());
+
+        for (Product product : productsList) {
+            System.out.println(product);
+        }
+
+    }
+}
